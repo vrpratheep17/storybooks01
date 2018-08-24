0a04031a35546b647166d2990c123449b1f7598d
pratheep <vrpratheep22@gmail.com>
2018-08-23 23:25:20 -0500

    second commit

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const express = require("express");
+const passport = require("passport");
 const mongoose = require("mongoose");
 const app = express();
-
+//passport config
+require("./config/passport")(passport);
 app.get("/", (req, res) => {
   res.send("it works");
 });
+//load routes
+const auth = require("./routes/auth");
+app.use("/auth", auth);
 
 const port = process.env.PORT || 3000;
 
---
+++ routes/auth.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const passport = require("passport");
+const router = express.Router();
+
+router.get(
+  "/google",
+  passport.authenticate("google", { scope: ["profile", "email"] })
+);
+router.get(
+  "/google/callback",
+  passport.authenticate("google", { failureRedirect: "/" }),
+  (req, res) => {
+    // Successful authentication, redirect home.
+    res.redirect("/dashboard");
+  }
+);
+
+module.exports = router;
